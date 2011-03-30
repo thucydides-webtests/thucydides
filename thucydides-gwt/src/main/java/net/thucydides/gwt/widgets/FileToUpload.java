c7e063754f611745773f685da0f171507796b1d3
John Ferguson Smart <john.smart@wakaleo.com>
2011-03-30 11:10:53 +1100

    If uploading files from a Windows client, the filename sent to the UI should be Windows-native (backslashes), not Java-style (with forward slashes), no matter what OS the server is running on.

--- thucydides-gwt/src/main/java/net/thucydides/gwt/widgets/FileToUpload.java
+++ thucydides-gwt/src/main/java/net/thucydides/gwt/widgets/FileToUpload.java
@@ -1,18 +1,43 @@
 package net.thucydides.gwt.widgets;
 
+import org.apache.commons.lang.StringUtils;
 import org.openqa.selenium.WebElement;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A class that helps upload a file to an HTML form in using a fluent API.
  */
 public class FileToUpload {
     private final String filename;
+    private final Pattern windowsPath = Pattern.compile("/[A-Z]:.*");
+
 
     public FileToUpload(final String filename) {
         this.filename = filename;
     }
 
     public void to(final WebElement uploadFileField) {
-         uploadFileField.sendKeys(filename);
+        uploadFileField.sendKeys(osSpecificPathOf(filename));
+    }
+
+
+    private String osSpecificPathOf(final String filename) {
+        if (isAWindows(filename)) {
+            return windowsNative(filename);
+        } else {
+            return filename;
+        }
+    }
+
+    private String windowsNative(final String filename) {
+        String bareFilename = filename.substring(1);
+        return StringUtils.replace(bareFilename,"/","\\");
+    }
+
+    private boolean isAWindows(final String filename) {
+        Matcher matcher = windowsPath.matcher(filename);
+        return matcher.matches();
     }
 }
--- thucydides-gwt/src/test/java/net/thucydides/gwt/WhenUploadingFiles.java
+++ thucydides-gwt/src/test/java/net/thucydides/gwt/WhenUploadingFiles.java
@@ -1,51 +1,61 @@
 package net.thucydides.gwt;
 
 import net.thucydides.gwt.pages.GwtShowcaseUploadPage;
-import net.thucydides.gwt.widgets.GwtButton;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import net.thucydides.gwt.widgets.FileToUpload;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.RenderedWebElement;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.ElementNotDisplayedException;
-
-import java.io.File;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 public class WhenUploadingFiles {
 
-    public static WebDriver driver;
-
     private GwtShowcaseUploadPage uploadPage;
 
-    @BeforeClass
-    public static void openBrowser() {
-        driver = new FirefoxDriver();
-        driver.get("http://gwt.google.com/samples/Showcase/Showcase.html#!CwFileUpload");
-    }
-    
-    @Before
-    public void setupPageObject() {
-        uploadPage = new GwtShowcaseUploadPage(driver);
-        uploadPage.waitForRenderedElements(By.xpath("//button"));
-    }
-    
     @Test
     public void should_upload_a_file_from_the_resources_directory() {
 
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://gwt.google.com/samples/Showcase/Showcase.html#!CwFileUpload");
+
+        uploadPage = new GwtShowcaseUploadPage(driver);
+        uploadPage.waitForRenderedElements(By.xpath("//button"));
+
         uploadPage.uploadFile("uploads/readme.txt");
         uploadPage.clickOkInAlertWindow();
-    }
 
-    @AfterClass
-    public static void closeBrowser() {
         driver.quit();
     }
+
+    @Test
+    public void should_convert_a_windows_java_path_to_a_proper_windows_path() {
+        String javaWindowsPath = "/C:/Users/MyUser/myproject/target/test-classes/documentUpload/somefile.pdf";
+        String realWindowsPath= "C:\\Users\\MyUser\\myproject\\target\\test-classes\\documentUpload\\somefile.pdf";
+
+        WebElement field = mock(WebElement.class);
+
+        FileToUpload fileToUpload = new FileToUpload(javaWindowsPath);
+        fileToUpload.to(field);
+
+        verify(field).sendKeys(realWindowsPath);
+    }
+
+
+    @Test
+    public void should_leave_a_unix_java_path_alone() {
+        String unixPath = "/home/myuser/target/test-classes/documentUpload/somefile.pdf";
+
+        WebElement field = mock(WebElement.class);
+
+        FileToUpload fileToUpload = new FileToUpload(unixPath);
+        fileToUpload.to(field);
+
+        verify(field).sendKeys(unixPath);
+    }
+
 }
