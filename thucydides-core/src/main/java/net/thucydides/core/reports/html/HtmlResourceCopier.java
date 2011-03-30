faf8482f0240d6437e520860f1aade4d745e8ab6
John Ferguson Smart <john.smart@wakaleo.com>
2011-03-31 07:25:23 +1100

    Removed redundant code

--- thucydides-core/src/main/java/net/thucydides/core/reports/html/HtmlResourceCopier.java
+++ thucydides-core/src/main/java/net/thucydides/core/reports/html/HtmlResourceCopier.java
@@ -54,69 +54,10 @@ public class HtmlResourceCopier {
         return !resourceDirectory.equals(resourcePath);
     }
 
-    private String findTargetSubDirectoryFrom(final String resourcePath) 
-      throws IOException {
-        if (resourcePath.startsWith(resourceDirectory)) {
-            int subDirectoryStartsAt = resourceDirectory.length() + 1;
-            String resourcePathName = resourcePath.substring(subDirectoryStartsAt);
-            if (resourcePathName.endsWith("/")) {
-                return resourcePathName;
-            } else {
-                File targetDirectory = new File(resourcePathName).getParentFile();
-                if (targetDirectory != null) {
-                    return targetDirectory.getPath();
-                }
-            }
-        }
-        return "";
-    }
-
     private Pattern allFilesInDirectory(final String directory) {
         String allFilesPattern = String.format(".*[\\\\/]?%s[\\\\/].*", directory);
         return Pattern.compile(allFilesPattern);
    }
-
-    private void copyFileFromClasspathToTargetDirectory(final String resourcePath,
-            final File targetDirectory) throws IOException {
-        
-        FileOutputStream out = null;
-        InputStream in = null;
-        
-        try {
-            File resourceOnClasspath = new File(resourcePath);
-            in = this.getClass().getClassLoader().getResourceAsStream(resourcePath);
-
-            File destinationFile = new File(targetDirectory, resourceOnClasspath.getName());
-            if (destinationFile.getParent() != null) {
-                new File(destinationFile.getParent()).mkdirs();
-            }
-            
-            out = new FileOutputStream(destinationFile);
-            
-            copyData(in, out); 
-            
-        } finally {
-            closeSafely(out, in);
-        }
-    }
-
-    private void copyData(final InputStream in, final OutputStream out) throws IOException {
-        byte[] buffer = new byte[BUFFER_SIZE];  
-        int bytesRead;  
-        while ((bytesRead = in.read(buffer)) != -1) {  
-            out.write(buffer, 0, bytesRead);  
-        }
-    }
-
-    private void closeSafely(final OutputStream out, final InputStream in)
-            throws IOException {
-        if (in != null) {
-            in.close();  
-        }
-        if (out != null) {
-            out.close(); 
-        }
-    }
 
     private boolean resourceIsFromAJar(final String resourcePath) {
         return !resourcePath.startsWith("/");
