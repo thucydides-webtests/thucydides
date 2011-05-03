2b8592af66d1ac9e4b91f997dfd9fddef2b10248
John Ferguson Smart <john.smart@wakaleo.com>
2011-05-03 20:23:53 +1000

    Refactoring

---
+++ thucydides-easyb/thucydides-easyb-plugin/src/main/groovy/net/thucydides/easyb/ThucydidesExecutionListener.groovy
@@ -0,0 +1,11 @@
+package net.thucydides.easyb
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johnsmart
+ * Date: 3/05/11
+ * Time: 4:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ThucydidesExecutionListener {
+}
---
+++ thucydides-easyb/thucydides-easyb-plugin/src/main/groovy/net/thucydides/easyb/ThucydidesListenerBuilder.groovy
@@ -0,0 +1,11 @@
+package net.thucydides.easyb
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johnsmart
+ * Date: 3/05/11
+ * Time: 4:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ThucydidesListenerBuilder {
+}
