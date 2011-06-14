0181bc084e3ffcf643c11c45c2ebc20f67b09d47
John Ferguson Smart <john.smart@wakaleo.com>
2011-06-14 13:40:50 +1000

    Fixed a test that was generating orphan browser windows.

--- thucydides-core/src/test/java/net/thucydides/core/webdriver/WhenInstanciatingANewDriver.java
+++ thucydides-core/src/test/java/net/thucydides/core/webdriver/WhenInstanciatingANewDriver.java
@@ -1,45 +1,39 @@
 package net.thucydides.core.webdriver;
 
+import net.thucydides.core.webdriver.mocks.TestableWebDriverFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import net.thucydides.core.webdriver.SupportedWebDriver;
-import net.thucydides.core.webdriver.mocks.TestableWebDriverFactory;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.openqa.selenium.WebDriver;
 
 public class WhenInstanciatingANewDriver {
 
     private TestableWebDriverFactory webDriverFactory;
-    
+
+    private WebDriver driver;
     @Before
     public void createATestableDriverFactory() {
         webDriverFactory = new TestableWebDriverFactory();
     }
+
+
+    @After
+    public void closeDriver() {
+        driver.quit();
+    }
     
     @Test
     public void should_support_creating_a_firefox_driver() {
-         WebDriver driver = webDriverFactory.newInstanceOf(SupportedWebDriver.FIREFOX);
+         driver = webDriverFactory.newInstanceOf(SupportedWebDriver.FIREFOX);
          assertThat(driver.toString(), containsString("FirefoxDriver"));
     }
 
     @Test
     public void should_support_creating_a_chrome_driver() {
-         WebDriver driver = webDriverFactory.newInstanceOf(SupportedWebDriver.CHROME);
+         driver = webDriverFactory.newInstanceOf(SupportedWebDriver.CHROME);
          assertThat(driver.toString(), containsString("ChromeDriver"));
     }
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-    
-    @Test
-    public void should_throw_exception_with_helpful_message_if_driver_unsupported() {
-         thrown.expect(IllegalArgumentException.class);
-         thrown.expectMessage("Driver type cannot be null");
-         webDriverFactory.newInstanceOf(null);
-    }
-    
 }
