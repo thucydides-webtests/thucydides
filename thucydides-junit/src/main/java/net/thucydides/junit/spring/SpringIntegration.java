3e916f874e02050e1a28940004066d32d34e66a2
John Ferguson Smart <john.smart@wakaleo.com>
2011-09-29 14:10:42 +1300

    Modified the code to be compatible with Spring 2.5.x as well as with Spring 3.0

--- thucydides-junit/src/main/java/net/thucydides/junit/spring/SpringIntegration.java
+++ thucydides-junit/src/main/java/net/thucydides/junit/spring/SpringIntegration.java
@@ -4,9 +4,6 @@ import org.junit.rules.MethodRule;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 import org.springframework.test.context.TestContextManager;
-import org.springframework.test.context.junit4.statements.RunAfterTestMethodCallbacks;
-import org.springframework.test.context.junit4.statements.RunBeforeTestMethodCallbacks;
-
 
 /**
  * Use the Spring test annotations in Thucydides tests.
