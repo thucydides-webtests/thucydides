2ed861e371e16fc303d14c850e1e2d9a80e76d24
John Ferguson Smart <john.smart@wakaleo.com>
2011-12-16 14:35:15 +1100

    Refactoring

--- thucydides-easyb/thucydides-easyb-plugin/src/test/groovy/net/thucydides/easyb/WhenUsingTheThucydidesEasybPlugin.groovy
+++ thucydides-easyb/thucydides-easyb-plugin/src/test/groovy/net/thucydides/easyb/WhenUsingTheThucydidesEasybPlugin.groovy
@@ -33,7 +33,6 @@ import java.util.concurrent.Callable
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
-import net.thucydides.easyb.samples.SampleSteps
 
 public class WhenUsingTheThucydidesEasybPlugin {
 
