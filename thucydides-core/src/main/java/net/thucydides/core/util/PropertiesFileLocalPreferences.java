8553b425a0d5506ae8d5abbab86991ca8ee35b88
--global <john.smart@wakaleo.com>
2012-04-27 07:52:46 +1000

    You can now define Thucydides properties in a thucydides.properties file in the working directory (#THUCYDIDES-61)

--- thucydides-core/src/main/java/net/thucydides/core/util/PropertiesFileLocalPreferences.java
+++ thucydides-core/src/main/java/net/thucydides/core/util/PropertiesFileLocalPreferences.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
- * Thucydides options can be loaded from the thucydides.properties file in the home directory.
+ * Thucydides options can be loaded from the thucydides.properties file in the home directory and/or in the working directory.
  * <p/>
  * User: johnsmart
  * Date: 24/12/11
@@ -17,6 +17,7 @@ import java.util.Properties;
  */
 public class PropertiesFileLocalPreferences implements LocalPreferences {
 
+    private File workingDirectory;
     private File homeDirectory;
     private final EnvironmentVariables environmentVariables;
 
@@ -24,6 +25,7 @@ public class PropertiesFileLocalPreferences implements LocalPreferences {
     public PropertiesFileLocalPreferences(EnvironmentVariables environmentVariables) {
         this.environmentVariables = environmentVariables;
         this.homeDirectory = new File(System.getProperty("user.home"));
+        this.workingDirectory = new File(System.getProperty("user.dir"));
     }
 
     public File getHomeDirectory() {
@@ -36,13 +38,19 @@ public class PropertiesFileLocalPreferences implements LocalPreferences {
 
     @Override
     public void loadPreferences() throws IOException {
-        File localPreferencesFile = getLocalPreferencesFile();
-        if (localPreferencesFile.exists()) {
+        File homeDirectoryPreferencesFile = getLocalPreferencesFile();
+        File workingDirectoryPreferencesFile = getLocalWorkingPreferencesFile();
+        updatePreferencesFrom(workingDirectoryPreferencesFile);
+        updatePreferencesFrom(homeDirectoryPreferencesFile);
+
+    }
+
+    private void updatePreferencesFrom(File preferencesFile) throws IOException {
+        if (preferencesFile.exists()) {
             Properties localPreferences = new Properties();
-            localPreferences.load(new FileInputStream(localPreferencesFile));
+            localPreferences.load(new FileInputStream(preferencesFile));
             setUndefinedSystemPropertiesFrom(localPreferences);
         }
-
     }
 
     private void setUndefinedSystemPropertiesFrom(Properties localPreferences) {
@@ -59,6 +67,10 @@ public class PropertiesFileLocalPreferences implements LocalPreferences {
 
     private File getLocalPreferencesFile() {
         return new File(homeDirectory, "thucydides.properties");
-
     }
+
+    private File getLocalWorkingPreferencesFile() {
+        return new File(workingDirectory, "thucydides.properties");
+    }
+
 }
