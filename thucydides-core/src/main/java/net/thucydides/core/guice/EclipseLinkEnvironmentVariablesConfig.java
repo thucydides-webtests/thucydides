ec18cd881b9867ebb3b05d4c9f584f5b4697dbf0
Rahul Jain <rahulj51@gmail.com>
2012-06-22 07:21:54 +0800

    Added Eclipselink support - work in progress

--- thucydides-core/src/main/java/net/thucydides/core/guice/EclipseLinkEnvironmentVariablesConfig.java
+++ thucydides-core/src/main/java/net/thucydides/core/guice/EclipseLinkEnvironmentVariablesConfig.java
@@ -4,6 +4,9 @@ import com.google.inject.Inject;
 import net.thucydides.core.statistics.database.LocalDatabase;
 import net.thucydides.core.util.EnvironmentVariables;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -14,6 +17,8 @@ import java.util.Properties;
  * To change this template use File | Settings | File Templates.
  */
 public class EclipseLinkEnvironmentVariablesConfig implements JPAProviderConfig {
+
+    private static final int TABLE_NAME_COLUMN  = 3;
 
     private final EnvironmentVariables environmentVariables;
     private final LocalDatabase localDatabase;
@@ -28,6 +33,55 @@ public class EclipseLinkEnvironmentVariablesConfig implements JPAProviderConfig 
 
     @Override
     public void setProperties(Properties properties) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        String driver = environmentVariables.getProperty("thucydides.statistics.driver_class", localDatabase.getDriver());
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ " + driver);
+        String url = environmentVariables.getProperty("thucydides.statistics.url", localDatabase.getUrl());
+        String username = environmentVariables.getProperty("thucydides.statistics.username", localDatabase.getUsername());
+        String password = environmentVariables.getProperty("thucydides.statistics.password", localDatabase.getPassword());
+        String dialect = environmentVariables.getProperty("thucydides.statistics.dialect", localDatabase.getDBPlatform());
+
+        properties.put("javax.persistence.jdbc.driver", driver);
+        properties.put("javax.persistence.jdbc.url", url);
+        properties.put("javax.persistence.jdbc.user", username);
+        properties.put("javax.persistence.jdbc.password", password);
+        properties.put("eclipselink.target-database", dialect);
+        properties.put("eclipselink.connection-pool.default.initial", "1");
+        properties.put("eclipselink.connection-pool.default.max", "1");
+
+        boolean databaseIsConfigured = databaseIsConfigured(properties);
+        if (isUsingLocalDatabase() || !databaseIsConfigured) {
+            properties.put("eclipselink.ddl-generation", "create-or-extend-tables"); //not yet supported till 2.4
+        } else {
+            properties.put("eclipselink.ddl-generation", "none");
+        }
+
+    }
+
+    private boolean databaseIsConfigured(Properties targetConfiguration) {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", targetConfiguration.getProperty("hibernate.connection.username"));
+        connectionProps.put("password", targetConfiguration.getProperty("hibernate.connection.password"));
+        String jdbcConnection = targetConfiguration.getProperty("hibernate.connection.url");
+        try {
+            Connection conn = DriverManager.getConnection(jdbcConnection, connectionProps);
+            List<String> tables = getTablesFrom(conn);
+            return tables.contains("TESTRUN");
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    private List<String> getTablesFrom(Connection conn) throws SQLException {
+        DatabaseMetaData md = conn.getMetaData();
+        ResultSet rs = md.getTables(null, null, "%", null);
+        List<String> tableNames = new ArrayList<String>();
+        while (rs.next()) {
+            tableNames.add(rs.getString(TABLE_NAME_COLUMN));
+        }
+        return tableNames;
+    }
+
+    public boolean isUsingLocalDatabase() {
+        return (environmentVariables.getProperty("thucydides.statistics.url") == null);
     }
 }
