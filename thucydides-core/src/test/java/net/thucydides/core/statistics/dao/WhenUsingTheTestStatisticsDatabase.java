b9b9dc587622c4497aebef8dd5152947adead89b
Rahul Jain <rahulj51@gmail.com>
2012-06-23 16:20:36 +0800

    Added a test for EclipseLink configuration

--- thucydides-core/src/test/java/net/thucydides/core/statistics/dao/WhenUsingTheTestStatisticsDatabase.java
+++ thucydides-core/src/test/java/net/thucydides/core/statistics/dao/WhenUsingTheTestStatisticsDatabase.java
@@ -4,10 +4,8 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
 import net.thucydides.core.Thucydides;
-import net.thucydides.core.guice.DatabaseConfig;
-import net.thucydides.core.guice.EnvironmentVariablesDatabaseConfig;
-import net.thucydides.core.guice.Injectors;
-import net.thucydides.core.guice.ThucydidesModule;
+import net.thucydides.core.ThucydidesSystemProperty;
+import net.thucydides.core.guice.*;
 import net.thucydides.core.pages.InternalSystemClock;
 import net.thucydides.core.pages.SystemClock;
 import net.thucydides.core.statistics.service.ClasspathTagProviderService;
@@ -70,6 +68,25 @@ public class WhenUsingTheTestStatisticsDatabase {
         assertThat((String)properties.get("hibernate.connection.url"), is("jdbc:hsqldb:mem:test"));
         assertThat((String)properties.get("hibernate.connection.username"), is("admin"));
         assertThat((String)properties.get("hibernate.connection.password"), is("password"));
+    }
+
+    @Test
+    public void should_be_able_to_define_statistics_database_via_eclipselink_system_properties() {
+
+        environmentVariables.setProperty(ThucydidesSystemProperty.JPA_PROVIDER.name(), JPAProvider.EclipseLink.name());
+        environmentVariables.setProperty("thucydides.statistics.driver_class", "org.hsqldb.jdbc.JDBCDriver");
+        environmentVariables.setProperty("thucydides.statistics.url", "jdbc:hsqldb:mem:test");
+        environmentVariables.setProperty("thucydides.statistics.username", "admin");
+        environmentVariables.setProperty("thucydides.statistics.password", "password");
+
+        HibernateTestOutcomeHistoryDAO dao = injector.getInstance(HibernateTestOutcomeHistoryDAO.class);
+
+        Map properties = dao.entityManagerFactory.getProperties();
+
+        assertThat((String)properties.get("javax.persistence.jdbc.driver"), is("org.hsqldb.jdbc.JDBCDriver"));
+        assertThat((String)properties.get("javax.persistence.jdbc.url"), is("jdbc:hsqldb:mem:test"));
+        assertThat((String)properties.get("javax.persistence.jdbc.user"), is("admin"));
+        assertThat((String)properties.get("javax.persistence.jdbc.password"), is("password"));
     }
 
 
