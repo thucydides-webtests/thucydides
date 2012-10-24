d056228f049c5b291a8ba1979e560cbc3483737c
--global <john.smart@wakaleo.com>
2012-10-24 22:00:05 +1100

    Extended the classfinder to deal with ckasses inside JAR dependencies as well

--- thucydides-core/src/main/java/net/thucydides/core/reflection/ClassFinder.java
+++ thucydides-core/src/main/java/net/thucydides/core/reflection/ClassFinder.java
@@ -1,11 +1,19 @@
 package net.thucydides.core.reflection;
 
+import com.google.common.collect.Lists;
+
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Load classes from a given package.
@@ -50,15 +58,15 @@ public class ClassFinder {
             URL resource = resources.nextElement();
             dirs.add(new File(resource.getFile()));
         }
-
-        List<Class<?>> classes = new ArrayList<Class<?>>();
+        Set<Class<?>> classes = new HashSet<Class<?>>();
         for (File directory : dirs) {
             classes.addAll(findClasses(directory, packageName));
+            classes.addAll(getClassesForPackage(packageName));
         }
-        return filtered(classes);
+        return filtered(new ArrayList<Class<?>>(classes));
     }
 
-    private List<Class<?>> filtered(List<Class<?>> classes) {
+    private List<Class<?>> filtered(List<? extends Class<?>> classes) {
         List<Class<?>> matchingClasses = new ArrayList<Class<?>>();
 
         for(Class clazz : classes) {
@@ -85,6 +93,72 @@ public class ClassFinder {
         }
     }
 
+    private List<Class<?>> getClassesForPackage(String packageName) {
+
+        PackageDirectory rootPackage = new PackageDirectory(packageName);
+        if (!rootPackage.exists()) {
+            return Collections.EMPTY_LIST;
+        } else {
+            List<Class<?>> classes = new ArrayList<Class<?>>();
+
+            if (rootPackage.directoryExists()) {
+                addClassesInPackageDirectory(rootPackage, classes);
+            } else {
+                addClassesFromJarAt(rootPackage, classes);
+            }
+            return classes;
+        }
+    }
+
+    private void addClassesFromJarAt(PackageDirectory rootPackage, List<Class<?>> classes) {
+        try {
+            JarFile jarFile = new JarFile(rootPackage.getJarPath());
+            Enumeration<JarEntry> entries = jarFile.entries();
+            while(entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String entryName = entry.getName();
+                if (entryIsAClassIn(rootPackage, entryName)) {
+                    try {
+                        classes.add(Class.forName(classNameForJarEntry(entryName)));
+                    }
+                    catch (ClassNotFoundException e) {
+                        throw new RuntimeException("Could not load class", e);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(rootPackage.getName() + " (" + rootPackage.getDirectory() + ") does not appear to be a valid package", e);
+        }
+    }
+
+    private boolean entryIsAClassIn(PackageDirectory rootPackage, String entryName) {
+        return (entryName.startsWith(rootPackage.getRelativePath())
+                && entryName.length() > (rootPackage.getRelativePath().length() + 1)
+                && entryName.endsWith(".class"));
+    }
+
+    private String classNameForJarEntry(String entryName) {
+        return entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
+    }
+
+    private void addClassesInPackageDirectory(PackageDirectory rootPackage, List<Class<?>> classes) {
+        for(String filename : rootPackage.getDirectory().list()) {
+            if (filename.endsWith(".class")) {
+                String className = classNameFor(rootPackage.getName(), filename);
+                try {
+                    classes.add(Class.forName(className));
+                }
+                catch (ClassNotFoundException e) {
+                    throw new RuntimeException("ClassNotFoundException loading " + className);
+                }
+            }
+        }
+    }
+
+    private String classNameFor(String packageName, String filename) {
+        return packageName + '.' + filename.substring(0, filename.length() - 6);
+    }
+
     /**
      * Recursive method used to find all classes in a given directory and subdirs.
      *
@@ -100,6 +174,7 @@ public class ClassFinder {
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
+                System.out.println("find classes in " + file);
                 if (file.isDirectory()) {
                     classes.addAll(findClasses(file, packageName + "." + file.getName()));
                 } else if (file.getName().endsWith(".class") && isNotAnInnerClass(file.getName())) {
@@ -134,4 +209,62 @@ public class ClassFinder {
     public ClassLoader getClassLoader() {
         return classLoader;
     }
+
+
+    private static class PackageDirectory {
+        private final Package rootPackage;
+        private final String packageName;
+        private final String relativePath;
+
+        private PackageDirectory(String packageName) {
+            this.packageName = packageName;
+            this.rootPackage = Package.getPackage(packageName);
+            this.relativePath =  packageName.replace('.','/');
+        }
+
+        public boolean exists() {
+            return (rootPackage != null);
+        }
+
+        public String getName() {
+            return packageName;
+        }
+
+        public String getRelativePath() {
+            return relativePath;
+        }
+
+        public String getFullPath() {
+            URL resource = ClassLoader.getSystemClassLoader().getResource(relativePath);
+            return resource.getFile();
+        }
+
+        public String getJarPath() {
+            return getFullPath().replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
+        }
+
+        public File getDirectory() {
+            URL resource = ClassLoader.getSystemClassLoader().getResource(relativePath);
+            try {
+                return new File(resource.toURI());
+            } catch (URISyntaxException e) {
+                throw new RuntimeException("Illegal URL for resource read from classpath: ",e);
+            }
+        }
+
+        public boolean isJarResource() {
+            URL resource = ClassLoader.getSystemClassLoader().getResource(relativePath);
+            try {
+                return resource.toURI().getScheme().equals("jar");
+            } catch (URISyntaxException e) {
+                throw new RuntimeException("Illegal URL for resource read from classpath: ",e);
+            }
+        }
+
+        public boolean directoryExists() {
+            return !isJarResource() && getDirectory().exists();
+        }
+
+    }
 }
+
--- thucydides-core/src/test/groovy/net/thucydides/core/reflection/WhenLoadingClassesFromAPackage.groovy
+++ thucydides-core/src/test/groovy/net/thucydides/core/reflection/WhenLoadingClassesFromAPackage.groovy
@@ -12,29 +12,29 @@ public class WhenLoadingClassesFromAPackage extends Specification {
         when:
             List<Class> classes = ClassFinder.loadClasses().fromPackage("net.thucydides.core.reflection.sampleclasses");
         then:
-            classes == [SomeClass, SomeOtherClass, SomeTestClass]
+            classes.contains(SomeTestClass) && classes.contains(SomeOtherClass) && classes.contains(SomeClass)
 
     }
 
     def "should load all classes in nested packages"() {
         when:
-        List<Class> classes = ClassFinder.loadClasses().fromPackage("net.thucydides.core.reflection");
+            List<Class> classes = ClassFinder.loadClasses().fromPackage("net.thucydides.core.reflection");
         then:
-        classes.size() >= 3
+            classes.size() >= 3
     }
 
     def "should load no classes if the package does not exist"() {
         when:
-        List<Class> classes = ClassFinder.loadClasses().fromPackage("that.does.not.exist");
+            List<Class> classes = ClassFinder.loadClasses().fromPackage("that.does.not.exist");
         then:
-        classes.isEmpty()
+            classes.isEmpty()
     }
 
     def "should not load resources on classpath"() {
         when:
-        List<Class> classes = ClassFinder.loadClasses().fromPackage("jquery");
+            List<Class> classes = ClassFinder.loadClasses().fromPackage("jquery");
         then:
-        classes.isEmpty()
+            classes.isEmpty()
     }
 
     def "should load classes with a given annotation"() {
@@ -44,6 +44,22 @@ public class WhenLoadingClassesFromAPackage extends Specification {
                                              .fromPackage("net.thucydides.core.reflection.sampleclasses");
         then:
             classes == [SomeTestClass]
+    }
+
+    def "should load all classes in a given package from a dependency"() {
+        when:
+            List<Class> classes = ClassFinder.loadClasses().fromPackage("spock.lang");
+        then:
+            !classes.isEmpty()
+
+    }
+
+    def "should load all nested classes in a given package from a dependency"() {
+        when:
+             List<Class> classes = ClassFinder.loadClasses().fromPackage("net.thucydides.core");
+        then:
+            !classes.isEmpty()
+
     }
 
 }
