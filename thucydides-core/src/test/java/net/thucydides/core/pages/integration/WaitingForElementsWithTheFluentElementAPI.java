dc523183b55428e7000a954ae7f8e6269c6adf07
--global <john.smart@wakaleo.com>
2014-04-23 08:11:41 +1000

    Minor improvements to the unit tests

--- thucydides-core/src/test/java/net/thucydides/core/pages/integration/WaitingForElementsWithTheFluentElementAPI.java
+++ thucydides-core/src/test/java/net/thucydides/core/pages/integration/WaitingForElementsWithTheFluentElementAPI.java
@@ -22,15 +22,20 @@ public class WaitingForElementsWithTheFluentElementAPI extends FluentElementAPIT
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
+    private static WebDriver phantomDriver;
     private static WebDriver driver;
 
     private static StaticSitePage staticPage;
+    private static StaticSitePage phantomPage;
 
     @BeforeClass
     public static void openBrowsers() {
-        driver = new FirefoxDriver();
+        driver = new  FirefoxDriver();
+        phantomDriver = new PhantomJSDriver(); 
         staticPage = new StaticSitePage(driver, 1000);
         staticPage.open();
+
+        phantomPage = new StaticSitePage(phantomDriver, 1000);
     }
 
     @AfterClass
@@ -38,19 +43,22 @@ public class WaitingForElementsWithTheFluentElementAPI extends FluentElementAPIT
         if (driver != null) {
             driver.quit();
         }
+        if (phantomDriver != null) {
+            phantomDriver.quit();
+        }
     }
 
+    protected StaticSitePage getPhantomJSPage() {
+        return phantomPage;
+    }
+    
     protected StaticSitePage getFirefoxPage() {
         return staticPage;
     }
 
-//    protected StaticSitePage getChromePage() {
-//        return chromePage;
-//    }
-
     @Test
     public void should_obtain_text_value_from_input() {
-        StaticSitePage page = getFirefoxPage();
+        StaticSitePage page = getPhantomJSPage();
         page.getDriver().navigate().refresh();
         assertThat(page.element(page.firstName).getValue(), is("<enter first name>"));
     }
@@ -101,7 +109,7 @@ public class WaitingForElementsWithTheFluentElementAPI extends FluentElementAPIT
 
     @Test
     public void should_wait_for_element_to_be_visible_and_enabled_before_clicking() {
-        StaticSitePage page = getFirefoxPage();
+        StaticSitePage page = getPhantomJSPage();
         page.open();
         //page.getDriver().navigate().refresh();
         page.element(page.checkbox).click();
@@ -118,7 +126,7 @@ public class WaitingForElementsWithTheFluentElementAPI extends FluentElementAPIT
 
     @Test
     public void should_be_able_to_build_composite_wait_until_disabled_clauses() throws InterruptedException {
-        StaticSitePage page = getFirefoxPage();
+        StaticSitePage page = getPhantomJSPage();
 
         page.waitForCondition().until(page.twoFieldsAreDisabled());
     }
@@ -127,7 +135,7 @@ public class WaitingForElementsWithTheFluentElementAPI extends FluentElementAPIT
     @Test
     @Ignore("Unreliable for current version of WebDriver")
     public void should_let_you_remove_the_focus_from_the_current_active_field() {
-        StaticSitePage page = getFirefoxPage();
+        StaticSitePage page = getPhantomJSPage();
 
         page.element(page.firstName).click();
 
@@ -139,7 +147,7 @@ public class WaitingForElementsWithTheFluentElementAPI extends FluentElementAPIT
 
     @Test
     public void should_wait_for_text_to_dissapear() {
-        StaticSitePage page = getFirefoxPage();
+        StaticSitePage page = getPhantomJSPage();
 
         page.waitForTextToDisappear("Dissapearing text");
         assertThat(page.containsText("Dissapearing text"), is(false));
@@ -148,19 +156,19 @@ public class WaitingForElementsWithTheFluentElementAPI extends FluentElementAPIT
 
     @Test
     public void contains_text_should_fail_if_text_is_not_present() {
-        StaticSitePage page = getFirefoxPage();
+        StaticSitePage page = getPhantomJSPage();
         assertThat(page.containsText("Not present"), is(false));
     }
 
     @Test
     public void contains_text_should_fail_if_text_is_invisible() {
-        StaticSitePage page = getFirefoxPage();
+        StaticSitePage page = getPhantomJSPage();
         assertThat(page.containsText("Invisible text"), is(false));
     }
 
     @Test
     public void should_wait_for_text_in_element_to_dissapear() {
-        StaticSitePage page = getFirefoxPage();
+        StaticSitePage page = getPhantomJSPage();
         page.waitForTextToDisappear(page.dissapearingtext, "Dissapearing text");
 
         assertThat(page.containsText("Dissapearing text"), is(false));
@@ -175,7 +183,7 @@ public class WaitingForElementsWithTheFluentElementAPI extends FluentElementAPIT
 
     @Test
     public void should_wait_for_field_to_be_enabled_using_alternative_style() throws InterruptedException {
-        StaticSitePage page = getFirefoxPage();
+        StaticSitePage page = getPhantomJSPage();
 
         page.firstName().waitUntilVisible();
         page.firstName().waitUntilEnabled();
