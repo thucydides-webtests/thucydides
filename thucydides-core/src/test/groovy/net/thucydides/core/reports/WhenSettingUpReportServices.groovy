17742d599aa0a94c49d757dbccfe8f87fe885b6d
--global <john.smart@wakaleo.com>
2014-07-29 17:21:42 +1000

    Added a test to illustrate how the ThucydidesReports helper class generates reports for test outcomes.

--- thucydides-core/src/test/groovy/net/thucydides/core/reports/WhenSettingUpReportServices.groovy
+++ thucydides-core/src/test/groovy/net/thucydides/core/reports/WhenSettingUpReportServices.groovy
@@ -1,12 +1,19 @@
 package net.thucydides.core.reports
 
+import com.github.goldin.spock.extensions.tempdir.TempDir
 import net.thucydides.core.ThucydidesReports
+import net.thucydides.core.model.Story
+import net.thucydides.core.model.TestOutcome
 import net.thucydides.core.util.MockEnvironmentVariables
 import net.thucydides.core.webdriver.SystemPropertiesConfiguration
 import org.openqa.selenium.WebDriver
 import spock.lang.Specification
 
 class WhenSettingUpReportServices extends Specification {
+
+
+    @TempDir
+    File outputDir;
 
     def environmentVariables = new MockEnvironmentVariables();
     def configuration = new SystemPropertiesConfiguration(environmentVariables);
@@ -38,6 +45,19 @@ class WhenSettingUpReportServices extends Specification {
             def listeners = ThucydidesReports.setupListeners(configuration)
         then:
             listeners.getResults() == listeners.baseStepListener.testOutcomes
+    }
+
+    def "should generate reports using each of the subscribed reporters"() {
+        given:
+            configuration.setOutputDirectory(outputDir)
+            ThucydidesReports.setupListeners(configuration)
+            def testOutcomes = [TestOutcome.forTestInStory("some test", Story.called("some story"))]
+        when:
+            ThucydidesReports.getReportService(configuration).generateReportsFor(testOutcomes)
+        then:
+            outputDir.list().findAll { it.endsWith(".html")}.size() == 1
+            outputDir.list().findAll { it.endsWith(".xml")}.size() == 1
+            outputDir.list().findAll { it.endsWith(".json")}.size() == 1
     }
 
 }
