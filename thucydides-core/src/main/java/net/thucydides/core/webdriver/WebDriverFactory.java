de34e73b836d994b4f3afef35aba5b7c336d196a
Imran <imran@browserstack.com>
2014-09-19 18:50:43 +0530

    Add support for BrowserStack specific capabilities

--- thucydides-core/src/main/java/net/thucydides/core/webdriver/WebDriverFactory.java
+++ thucydides-core/src/main/java/net/thucydides/core/webdriver/WebDriverFactory.java
@@ -151,7 +151,11 @@ public class WebDriverFactory {
     public boolean usesSauceLabs() {
         return StringUtils.isNotEmpty(ThucydidesSystemProperty.SAUCELABS_URL.from(environmentVariables));
     }
-
+    
+    public boolean usesBrowserStack() {
+        return StringUtils.isNotEmpty(ThucydidesSystemProperty.BROWSERSTACK_URL.from(environmentVariables));
+    }
+    
     /**
      * This method is synchronized because multiple webdriver instances can be created in parallel.
      * However, they may use common system resources such as ports, so may potentially interfere
@@ -163,8 +167,8 @@ public class WebDriverFactory {
     protected synchronized WebDriver newWebdriverInstance(final Class<? extends WebDriver> driverClass) {
         try {
             WebDriver driver;
-            if (isARemoteDriver(driverClass) || shouldUseARemoteDriver() || saucelabsUrlIsDefined()) {
-                driver = newRemoteDriver();
+            if (isARemoteDriver(driverClass) || shouldUseARemoteDriver() || saucelabsUrlIsDefined() || browserStackUrlIsDefined()) {
+            	driver = newRemoteDriver();
             } else if (isAFirefoxDriver(driverClass)) {
                 driver = firefoxDriver();
             } else if (isAnHtmlUnitDriver(driverClass)) {
@@ -255,9 +259,12 @@ public class WebDriverFactory {
     }
 
     private WebDriver  newRemoteDriver() throws MalformedURLException {
+
         WebDriver driver = null;
         if (saucelabsUrlIsDefined()) {
             driver = buildSaucelabsDriver();
+        } else if (browserStackUrlIsDefined()){
+        	driver = buildBrowserStackDriver();
         } else {
             driver = buildRemoteDriver();
         }
@@ -274,6 +281,10 @@ public class WebDriverFactory {
         return ThucydidesSystemProperty.SAUCELABS_URL.isDefinedIn(environmentVariables);
     }
 
+    private boolean browserStackUrlIsDefined() {
+        return ThucydidesSystemProperty.BROWSERSTACK_URL.isDefinedIn(environmentVariables);
+    }
+    
     private WebDriver buildSaucelabsDriver() throws MalformedURLException {
         String saucelabsUrl = ThucydidesSystemProperty.SAUCELABS_URL.from(environmentVariables);
         WebDriver driver = webdriverInstanceFactory.newRemoteDriver(new URL(saucelabsUrl), findSaucelabsCapabilities());
@@ -285,6 +296,13 @@ public class WebDriverFactory {
         }
         return driver;
     }
+    
+    private WebDriver buildBrowserStackDriver() throws MalformedURLException{
+    	String browserStackUrl = ThucydidesSystemProperty.BROWSERSTACK_URL.from(environmentVariables);
+        WebDriver driver = webdriverInstanceFactory.newRemoteDriver(new URL(browserStackUrl), findbrowserStackCapabilities());
+        return driver;
+    }
+    
 
     public static String getDriverFrom(EnvironmentVariables environmentVariables, String defaultDriver) {
         String driver = getDriverFrom(environmentVariables);
@@ -313,6 +331,47 @@ public class WebDriverFactory {
         capabilities.setJavascriptEnabled(true);
 
         return capabilities;
+    }
+    
+    
+    private Capabilities findbrowserStackCapabilities() {
+
+    	String driver = getDriverFrom(environmentVariables);
+    	DesiredCapabilities capabilities = capabilitiesForDriver(driver);
+    	
+    	configureBrowserStackOs(capabilities);
+    	
+    	configureBrowserStackOsVersion(capabilities);
+    	
+    	configureBrowserStackBrowser(capabilities);
+
+    	configureBrowserStackBrowserVersion(capabilities);
+
+        configureBrowserStackDevice(capabilities);
+
+        configureBrowserStackDeviceOrientation(capabilities);
+
+        configureBrowserStackProject(capabilities);
+
+        configureBrowserStackBuild(capabilities);
+
+        configureBrowserStackName(capabilities);
+
+        configureBrowserStackLocal(capabilities);
+
+        configureBrowserStackDebug(capabilities);
+
+        configureBrowserStackResolution(capabilities);
+
+        configureBrowserStackSeleniumVersion(capabilities);
+
+        configureBrowserStackIeNoFlash(capabilities);
+
+        configureBrowserStackIeDriver(capabilities);
+
+        configureBrowserStackIeEnablePopups(capabilities);
+
+    	return capabilities;
     }
 
     private void configureBrowserVersion(DesiredCapabilities capabilities) {
@@ -365,6 +424,7 @@ public class WebDriverFactory {
         }
     }
 
+    
     private String bestGuessOfTestName() {
         for (StackTraceElement elt : Thread.currentThread().getStackTrace()) {
             try {
@@ -380,6 +440,118 @@ public class WebDriverFactory {
             }
         }
         return null;
+    }
+
+    private void configureBrowserStackOs(DesiredCapabilities capabilities) {
+    	String os = ThucydidesSystemProperty.BROWSERSTACK_OS.from(environmentVariables);
+    	if (isNotEmpty(os)) {
+            capabilities.setCapability("os", os);
+        }
+    }
+    
+    private void configureBrowserStackOsVersion(DesiredCapabilities capabilities) {
+    	String os_version = ThucydidesSystemProperty.BROWSERSTACK_OS_VERSION.from(environmentVariables);
+    	if (isNotEmpty(os_version)) {
+            capabilities.setCapability("os_version", os_version);
+        }
+    }
+
+    private void configureBrowserStackBrowser(DesiredCapabilities capabilities){
+    	String browser = ThucydidesSystemProperty.BROWSERSTACK_BROWSER.from(environmentVariables);
+    	if (isNotEmpty(browser)) {
+    		capabilities.setCapability("browser", browser);
+    	}
+    }
+    
+    private void configureBrowserStackBrowserVersion(DesiredCapabilities capabilities){
+    	String browserVersion = ThucydidesSystemProperty.BROWSERSTACK_BROWSER_VERSION.from(environmentVariables);
+    	if (isNotEmpty(browserVersion)) {
+    		capabilities.setCapability("browser_version", browserVersion);
+    	}
+    }
+
+    private void configureBrowserStackDevice(DesiredCapabilities capabilities){
+        String device = ThucydidesSystemProperty.BROWSERSTACK_DEVICE.from(environmentVariables);
+        if (isNotEmpty(device)) {
+            capabilities.setCapability("device", device);
+        }
+    }
+
+    private void configureBrowserStackDeviceOrientation(DesiredCapabilities capabilities){
+        String deviceOrientation = ThucydidesSystemProperty.BROWSERSTACK_DEVICE_ORIENTATION.from(environmentVariables);
+        if (isNotEmpty(deviceOrientation)) {
+            capabilities.setCapability("deviceOrientation", deviceOrientation);
+        }
+    }
+
+    private void configureBrowserStackProject(DesiredCapabilities capabilities){
+        String project = ThucydidesSystemProperty.BROWSERSTACK_PROJECT.from(environmentVariables);
+        if (isNotEmpty(project)) {
+            capabilities.setCapability("project", project);
+        }
+    }
+
+    private void configureBrowserStackBuild(DesiredCapabilities capabilities){
+        String build = ThucydidesSystemProperty.BROWSERSTACK_BUILD.from(environmentVariables);
+        if (isNotEmpty(build)) {
+            capabilities.setCapability("build", build);
+        }
+    }
+
+    private void configureBrowserStackName(DesiredCapabilities capabilities){
+        String name = ThucydidesSystemProperty.BROWSERSTACK_SESSION_NAME.from(environmentVariables);
+        if (isNotEmpty(name)) {
+            capabilities.setCapability("name", name);
+        }
+    }
+
+    private void configureBrowserStackLocal(DesiredCapabilities capabilities){
+        String local = ThucydidesSystemProperty.BROWSERSTACK_LOCAL.from(environmentVariables);
+        if (isNotEmpty(local)) {
+            capabilities.setCapability("browserstack.local", local);
+        }
+    }
+
+    private void configureBrowserStackDebug(DesiredCapabilities capabilities){
+        String debug = ThucydidesSystemProperty.BROWSERSTACK_DEBUG.from(environmentVariables);
+        if (isNotEmpty(debug)) {
+            capabilities.setCapability("browserstack.debug", debug);
+        }
+    }
+
+    private void configureBrowserStackResolution(DesiredCapabilities capabilities){
+        String resolution = ThucydidesSystemProperty.BROWSERSTACK_RESOLUTION.from(environmentVariables);
+        if (isNotEmpty(resolution)) {
+            capabilities.setCapability("browserstack.resolution", resolution);
+        }
+    }
+
+    private void configureBrowserStackSeleniumVersion(DesiredCapabilities capabilities){
+        String seleniumVersion = ThucydidesSystemProperty.BROWSERSTACK_SELENIUM_VERSION.from(environmentVariables);
+        if (isNotEmpty(seleniumVersion)) {
+            capabilities.setCapability("browserstack.selenium_version", seleniumVersion);
+        }
+    }
+
+    private void configureBrowserStackIeNoFlash(DesiredCapabilities capabilities){
+        String ieNoFlash = ThucydidesSystemProperty.BROWSERSTACK_IE_NO_FLASH.from(environmentVariables);
+        if (isNotEmpty(ieNoFlash)) {
+            capabilities.setCapability("browserstack.ie.noFlash", ieNoFlash);
+        }
+    }
+
+    private void configureBrowserStackIeDriver(DesiredCapabilities capabilities){
+        String ieDriver = ThucydidesSystemProperty.BROWSERSTACK_IE_DRIVER.from(environmentVariables);
+        if (isNotEmpty(ieDriver)) {
+            capabilities.setCapability("browserstack.ie.driver", ieDriver);
+        }
+    }
+
+    private void configureBrowserStackIeEnablePopups(DesiredCapabilities capabilities){
+        String ieEnablePopups = ThucydidesSystemProperty.BROWSERSTACK_IE_ENABLE_POPUPS.from(environmentVariables);
+        if (isNotEmpty(ieEnablePopups)) {
+            capabilities.setCapability("browserstack.ie.enablePopups", ieEnablePopups);
+        }
     }
 
     private boolean isATestMethod(Method callingMethod) {
